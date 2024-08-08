43b81171cc69165e8beda987f876e59a1fa4d0d1
MuhammadQasimFiaz <80813627+MuhammadQasimFiaz@users.noreply.github.com>
2024-08-08 16:31:47 +0500

    add go to top button

--- assets/js/main.js
+++ assets/js/main.js
@@ -166,3 +166,23 @@ function adjustContainerClasses() {
 adjustContainerClasses();
 
 window.addEventListener('resize', adjustContainerClasses);
+
+document.addEventListener('DOMContentLoaded', () => {
+// Get the button
+let goToTopBtn = document.getElementById("goToTopBtn");
+
+// Show the button when the user scrolls down 20px from the top of the document
+window.onscroll = function() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        goToTopBtn.style.display = "block";
+    } else {
+        goToTopBtn.style.display = "none";
+    }
+};
+
+// When the user clicks on the button, scroll to the top of the document
+goToTopBtn.addEventListener('click', function() {
+    window.scrollTo({top: 0, behavior: 'smooth'});
+});
+})
+
